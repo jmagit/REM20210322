b5c78d859297a1ce78d63bf81f8d952dd4cac887
Javier <jmartin@grupoloyal.com>
2021-03-22 11:57:11 +0100

    JavaDoc

---
+++ demos/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.examples","c":"Principal","l":"divide(double, double)","u":"divide(double,double)"},{"p":"com.examples","c":"Principal","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.examples","c":"Principal","l":"Principal()","u":"%3Cinit%3E()"}];updateSearchResults();
--- demos/src/com/examples/Principal.java
+++ demos/src/com/examples/Principal.java
@@ -1,9 +1,31 @@
 package com.examples;
 
+/**
+ * Primera clase de ejemplo
+ * @author Javier
+ *
+ */
 public class Principal {
 
+	/**
+	 * Método principal
+	 * @param args Linea de argumentos del comando
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 	}
 
+	/**
+	 * División real de dos números
+	 * @param a Primer operando 
+	 * @param b Segundo operando, no puede ser 0
+	 * @return Resultado de la division real
+	 * @throws ArithmeticException No se puede dividir por 0
+	 */
+	public double divide(double a, double b) throws ArithmeticException  {
+		if(b == 0)
+			throw new ArithmeticException();
+		return a / b;
+		
+	}
 }
