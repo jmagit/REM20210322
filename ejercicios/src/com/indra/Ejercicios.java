eadee079a51ec752d43ade0cb5d3ec542ba8fd48
Javier <jmartin@grupoloyal.com>
2021-03-24 10:11:41 +0100

    Solucion

--- ejercicios/src/com/indra/Ejercicios.java
+++ ejercicios/src/com/indra/Ejercicios.java
@@ -6,17 +6,71 @@ import java.util.Scanner;
 public class Ejercicios {
 
 	public static void main(String[] args) {
-		juego();
+		// juego();
+		calcula("3+4+3,4-7*1=");
+	}
+
+	private static java.util.Scanner teclado = new java.util.Scanner(System.in);
+
+	private static String leer() {
+		return teclado.nextLine();
 	}
 
 	public static void juego() {
-		System.out.println("estoy jugando");
-		Scanner teclado = new Scanner(System.in);
-		var cad = teclado.nextLine();
-		var rnd = new Random();
-		var num = rnd.nextInt(10);
-		System.out.println(Integer.parseInt(cad));
-		System.out.println(cad);
+		do {
+			var rnd = new Random();
+			int numeroBuscado = rnd.nextInt(100);
+			int numeroIntroducido;
+			int intentos = 0;
+			boolean encontrado = false;
+			do {
+				System.out.print("Dame tu numero (" + (intentos + 1) + " de 10): ");
+				numeroIntroducido = Integer.parseInt(leer());
+				intentos += 1;
+				if (numeroBuscado == numeroIntroducido) {
+					encontrado = true;
+				} else if (numeroBuscado > numeroIntroducido) {
+					System.out.println("Mi número es mayor.");
+				} else {
+					System.out.println("Mi número es menor.");
+				}
+			} while (intentos < 10 && !encontrado);
+
+			if (encontrado) {
+				System.out.println("Bieeen!!! Acertaste.");
+			} else {
+				System.out.println("Upsss! Se acabaron los intentos, el número era el " + numeroBuscado);
+			}
+			System.out.println("¿Otra? [S/N]");
+		} while ("s".compareToIgnoreCase(leer()) == 0);
+	}
+
+	public static double calcula(String expresion) {
+		String operando = "";
+		for (int i = 0; i < expresion.length(); i++) {
+			char ch = expresion.charAt(i);
+			if (Character.isDigit(ch)) {
+				operando += ch;
+			} else if (ch == ',') {
+				if (operando.indexOf(ch) == -1) {
+					operando += ch;
+				} else {
+					// throw new Exception("Ya existe separador decimal.");
+				}
+			} else if ("+-*/%=".indexOf(ch) >= 0) {
+				if (operando.endsWith(",")) {
+					operando += "0";
+				}
+				System.out.println(operando + " " + ch);
+				if (ch == '=') {
+					break;
+				}
+				operando = "";
+			} else if (ch != ' ') {
+//				throw new Exception("Caracter no valido.");
+			}
+		}
+		return 0;
 	}
 
 }
